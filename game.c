671824b572d2b7778c4c94f454de9a668a09d1ad
Falko Goettsch <mr.falko@gmail.com>
2018-09-23 09:52:01 -0700

    adopted classic MUD terminology, clearer var names, functionified moving, added flava

--- game.c
+++ game.c
@@ -2,32 +2,60 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
+#define WALL '#'
+#define FLOOR '.'
+
+#define ICON_HUMAN '@'
+#define ICON_GOBLIN 'o'
+#define ICON_ORC 'O'
+
+#define OUCH '!'
+
 #define LEVEL_WIDTH  200
 #define LEVEL_HEIGHT 100
+
 char level[LEVEL_HEIGHT][LEVEL_WIDTH];
 
 enum Behavior {RandomWalk};
-struct dood {
+
+struct mobile {
     char display;
     int x;
     int y;
     bool active;
     enum Behavior behavior;
+    char emote;
 };
 
-struct dood player;
+struct mobile player;
 
-#define MAX_DOODS 1000
-struct dood doods[MAX_DOODS];
+#define MAX_MOBS 100
+struct mobile mobs[MAX_MOBS];
 
 bool is_position_valid(int x, int y) {
-    return level[y][x] != '#';
+    return level[y][x] != WALL;
 }
 
-void draw_dood(struct dood *d, int dx, int dy) {
-    mvprintw(dy+d->y, dx+d->x, "%c", d->display);
+bool move_if_valid(struct mobile *mob, int x, int y) {
+    if (is_position_valid(x, y)) {
+        mob->x = x;
+        mob->y = y;
+        return true;
+    } else {
+        return false;
+    }
 }
 
+void draw_mobile(struct mobile *mob, int dx, int dy) {
+    char display = mob->display;
+
+    if (mob->emote) {
+        display = mob->emote;
+        mob->emote = false;
+    }
+
+    mvprintw(dy+mob->y, dx+mob->x, "%c", display);
+}
 void draw() {
     int row,col;
     getmaxyx(stdscr,row,col);
@@ -46,42 +74,41 @@ void draw() {
             }
         }
     }
-    for (int i=0; i < MAX_DOODS; i++) {
-        if (doods[i].active) {
-            draw_dood(&doods[i], dx, dy);
+    for (int i=0; i < MAX_MOBS; i++) {
+        if (mobs[i].active) {
+            draw_mobile(&mobs[i], dx, dy);
         }
     }
-    draw_dood(&player, dx, dy);
+    draw_mobile(&player, dx, dy);
 }
 
 void setup_level() {
     for (int x = 0; x < LEVEL_WIDTH; x++) {
         for (int y = 0; y < LEVEL_HEIGHT; y++) {
             if (x == 0 || y == 0 || y == LEVEL_HEIGHT-1 || x == LEVEL_WIDTH-1) {
-                level[y][x] = '#';
+                level[y][x] = WALL;
             } else {
-                level[y][x] = '.';
+                level[y][x] = FLOOR;
             }
         }
     }
     player.x = player.y = 1;
-    player.display = '@';
-    for (int i=0; i < MAX_DOODS; i++) {
-        doods[i].active = false;
+    player.display = ICON_HUMAN;
+    for (int i=0; i < MAX_MOBS; i++) {
+        mobs[i].active = false;
     }
 
     for (int i=0; i < 100; i++) {
-        doods[i].x = rand()%(LEVEL_WIDTH-2) + 1;
-        doods[i].y = rand()%(LEVEL_HEIGHT-2) + 1;
-        doods[i].behavior = RandomWalk;
-        doods[i].active = true;
+        mobs[i].x = rand()%(LEVEL_WIDTH-2) + 1;
+        mobs[i].y = rand()%(LEVEL_HEIGHT-2) + 1;
+        mobs[i].behavior = RandomWalk;
+        mobs[i].active = true;
 
         if (rand()%2 == 0) {
-            doods[i].display = 'o';
+            mobs[i].display = ICON_GOBLIN;
         } else {
-            doods[i].display = 'O';
+            mobs[i].display = ICON_ORC;
         }
-
     }
 }
 
@@ -105,17 +132,14 @@ int move_player() {
             return -1;
     }
 
-    if (is_position_valid(x, y)) {
-        player.x = x;
-        player.y = y;
-    }
+    move_if_valid(&player, x, y);
     return 0;
 }
 
-void move_dood(struct dood *d) {
-    int x = d->x;
-    int y = d->y;
-    switch (d->behavior) {
+void move_mobile(struct mobile *mob) {
+    int x = mob->x;
+    int y = mob->y;
+    switch (mob->behavior) {
         case RandomWalk:
             if (rand()%2 == 0) {
                 x += rand()%3 - 1;
@@ -123,9 +147,8 @@ void move_dood(struct dood *d) {
                 y += rand()%3 - 1;
             }
     }
-    if (is_position_valid(x, y)) {
-        d->x = x;
-        d->y = y;
+    if (!(move_if_valid(mob, x, y))) {
+        mob->emote = OUCH;
     }
 }
 
@@ -143,14 +166,13 @@ void main() {
         setup_level();
         draw();
         while (move_player() == 0) {
-            for (int i=0; i < MAX_DOODS; i++) {
-                if (doods[i].active) {
-                    move_dood(&doods[i]);
+            for (int i=0; i < MAX_MOBS; i++) {
+                if (mobs[i].active) {
+                    move_mobile(&mobs[i]);
                 }
             }
             draw();
         }
-
 
         endwin();
 }
